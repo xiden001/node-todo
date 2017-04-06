d90f4b95e3e548eba5fdea4f7ead9ae46dd8e56a
xiden001 <samuel.adoga1@gmail.com>
2017-04-06 21:41:44 +0100

    Modified Database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://xiden001:torque007@ec2-54-202-26-231.us-west-2.compute.amazonaws.com:27017/dummyDB',
-    localUrl: 'mongodb://xiden001:torque007@ec2-54-202-26-231.us-west-2.compute.amazonaws.com:27017/dummyDB'
+    remoteUrl : 'mongodb://xiden:torque007@ec2-54-202-26-231.us-west-2.compute.amazonaws.com:27017/dummyDB',
+    localUrl: 'mongodb://xiden:torque007@ec2-54-202-26-231.us-west-2.compute.amazonaws.com:27017/dummyDB'
 };
