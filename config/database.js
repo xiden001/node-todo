cff36807d45c0b991ba9776adcb0de285ec4de25
xiden001 <samuel.adoga1@gmail.com>
2017-04-06 21:23:19 +0100

    Edited Database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://xiden001:torque007@ec2-54-202-26-231.us-west-2.compute.amazonaws.com:27017/dummyDB',
+    localUrl: 'mongodb://xiden001:torque007@ec2-54-202-26-231.us-west-2.compute.amazonaws.com:27017/dummyDB'
 };
